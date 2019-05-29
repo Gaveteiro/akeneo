eb6f05535d9b95cbfe8134f81859f1f547d1ee9f
Arnaldo Miciano <arnaldo.miciano@gaveteiro.com.br>
2019-05-29 14:26:22 -0300

    Inclusão de método para criar uma lista de opções.

--- lib/akeneo/attribute_service.rb
+++ lib/akeneo/attribute_service.rb
@@ -38,5 +38,9 @@ module Akeneo
     def create_option(attribute_code, json)
       post_request("/attributes/#{attribute_code}/options", body: JSON.generate(json))
     end
+
+    def create_several(attribute_code, json)
+      patch_for_collection_request("/attributes/#{attribute_code}/options", body: json)
+    end
   end
 end

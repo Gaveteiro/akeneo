c1a3d057c1fd7d883aa6949ac0019dc108a38746
pedro <pedro.ferreira@gaveteiro.com.br>
2019-07-26 16:21:03 -0300

    adding entity service with create_sereral method

---
+++ lib/akeneo/entity_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative './service_base.rb'
+
+module Akeneo
+  class EntityService < ServiceBase
+    def initialize(url:, access_token:)
+      @url = url
+      @access_token = access_token
+
+    end
+
+    def create_several_entity_records(entity_name, entity_records)
+      patch_for_collection_request("/reference-entities/#{entity_name}/records", body: entity_records)
+    end
+  end
+end
